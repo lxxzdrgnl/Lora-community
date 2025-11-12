596b494c2508f33b324880a0d0de940a9b36ee06
lxxzdrgnl <pung4905@naver.com>
2025-11-12 18:12:21 +0900

    init

---
+++ src/main/java/rheon/wsd_lora_community/WsdLoraCommunityApplication.java
@@ -0,0 +1,13 @@
+package rheon.wsd_lora_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdLoraCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsdLoraCommunityApplication.class, args);
+    }
+
+}
