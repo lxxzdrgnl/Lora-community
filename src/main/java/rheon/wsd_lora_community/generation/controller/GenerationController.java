7f17464e5c8f53bcc4f27cd38f7fe41179fa80a8
lxxzdrgnl <pung4905@naver.com>
2025-11-20 21:37:15 +0900

    Fix: Modify call back url

--- src/main/java/rheon/wsd_lora_community/generation/controller/GenerationController.java
+++ src/main/java/rheon/wsd_lora_community/generation/controller/GenerationController.java
@@ -92,7 +92,7 @@ public class GenerationController {
         String s3Uri = "s3://lora-models-bucket/" + startResponse.getS3Key();
 
         // Callback URL 설정
-        String callbackUrl = "http://localhost:8080/api/generate/history";
+        String callbackUrl = "http://bluemingai.ap-northeast-2.elasticbeanstalk.com/api/generate/history";
 
         // FastAPI/Modal로 이미지 생성 요청 (비동기)
         fastApiClient.startImageGeneration(
